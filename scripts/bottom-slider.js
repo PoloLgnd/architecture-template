d34099722917c8062fc2a2b477a5793bcd45a643
Polo <159186068+PoloLgnd@users.noreply.github.com>
2025-06-02 14:57:21 +0300

    Поломанные кнопочки

--- scripts/bottom-slider.js
+++ scripts/bottom-slider.js
@@ -2,11 +2,23 @@
     const SLIDES = document.querySelectorAll('.buttom-slide');
 	const DOTS = document.querySelectorAll('.swiching-pag1');
 	const SLIDER = document.querySelectorAll('.main-buttom-news');
-	const ARROWS = document.querySelectorAll('.arrow');
+	const ARROW_LEFT = document.querySelectorAll('.prev-arrow');
+	const ARROW_RIGHT = document.querySelectorAll('.next-arrow')
 	let active_slide = 0;
 
+	ARROW_LEFT.addEventListener('click', () =>{
+		if (active_slide !== 0) active_slide -= 1;
+		changeDots();
+		changeSlide();
+	})
+	
+	ARROW_RIGHT.addEventListener('click', () =>{
+		if (active_slide !== (SLIDES.length - 1)) active_slide += 1;
+		changeDots();
+		changeSlide();
+	})
 
-	[...DOTS].forEach((pag, index) => {
+	DOTS.forEach((pag, index) => {
 		pag.addEventListener('click', function() {
 			if (!pag.classList.contains('active')) {
 				// clearInterval(timer);
@@ -26,10 +38,6 @@
 		});
 	}
 
-	function clickArrows() {
-		
-	}
-
 	function changeSlide() {
 		[...SLIDES].forEach((slide, idx) => {
 			if (idx === active_slide) {
@@ -44,6 +52,23 @@
 					slide.classList.add('next');
 				}
 			}
+
+			disableArrow();
+
 		})
+	}
+
+	function disableArrow() {
+		if (active_slide === 0) {
+			ARROW_LEFT.classList.add('disabled');
+		} else {
+			if (ARROW_LEFT.classList.contains('disabled')) ARROW_LEFT.classList.remove('disabled');
+		}
+
+		if (active_slide === (SLIDES.length - 1)) {
+			ARROW_RIGHT.classList.add('disabled');	
+		} else {
+			if (ARROW_RIGHT.classList.contains('disabled')) ARROW_RIGHT.classList.remove('disabled');
+		}
 	}
 })();
