36212b543ed43d4645f845c271f6d1365c5df3a3
Polo <159186068+PoloLgnd@users.noreply.github.com>
2025-06-13 20:54:55 +0300

    Сделал мобильное меню
    Сделал мобильное меню, но не всё, ещё надо поработать над прозрачностью

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -1,6 +1,8 @@
 const MENU_BTN = document.querySelector('.btn-menu_mobile');
 const MENU_MOBILE = document.querySelector('.menu-mobile');
+const MAIN_CONTENT = document.querySelector('main');
 
 MENU_BTN.addEventListener('click', () => {
     MENU_MOBILE.classList.toggle('hidden');
+    MAIN_CONTENT.classList.toggle('hiddeni');
 })
