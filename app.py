e25ef7450c94b0be193fefeb91f3c88e7aaf6c90
Polo <159186068+PoloLgnd@users.noreply.github.com>
2026-01-15 15:56:53 +0300

    сделал загрузку файла css

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/1.html')
-def index():
+def home():
     return render_template("1.html")
 
 
@@ -13,9 +13,16 @@ def about():
     return render_template("2.html")
 
 
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
+
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return("user page" + " - " + name + " - " + str(id))
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+# 13 минута - шаблоны для сайтов 
