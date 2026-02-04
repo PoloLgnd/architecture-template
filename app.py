57520ae02846ea8a013857da12f6581459b0793d
Polo <159186068+PoloLgnd@users.noreply.github.com>
2026-02-04 12:26:21 +0300

    Финальная версия для практики
    Тут я пытался сделать новую таблицу с готовыми почтами, которым отправили письмо, но у меня кучу проблем в моменте вылетело и я, на этом этапе, от такой идеи отказался, по крайней мере на сейчас, возможно я ещё к этой идее вернусь.

--- app.py
+++ app.py
@@ -13,12 +13,22 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default = datetime.utcnow)
 
 
-
     def __repr__(self):
         return "<Article %r>" % self.id
+    
+
+class ProcessedEmail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    original_id = db.Column(db.Integer)
+    processed_date = db.Column(db.DateTime, default = datetime.utcnow)
 
 
+    def __repr__(self):
+        return f"<ProcessedEmail {self.id}>"
 
+
+# здесь мы начинает кодить ту часть сайта, что отвечает за сбор почт на БД
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/1.html', methods=['POST', 'GET'])
@@ -45,8 +55,28 @@ def home():
     return render_template("1.html")
 
 
-@app.route('/2.html')
-def about():
+@app.route('/2.html/', methods=['POST', 'GET'])
+@app.route('/2.html', methods=['POST', 'GET'])
+def second_home():
+    if request.method == "POST":
+        if 'userEmail' not in request.form:
+            return "Поле email не найдено в запросе"
+        
+        userEmail = request.form['userEmail']
+        
+        if not userEmail:
+            return "Email не может быть пустым"
+        
+        mail_entry = Article(email=userEmail)
+        
+        try:
+            db.session.add(mail_entry)
+            db.session.commit()
+            return redirect('/2.html')
+        except Exception as e:
+            app.logger.error(f'Ошибка при добавлении почты: {e}')
+            return "При добавлении почты возникла ошибка :("
+    
     return render_template("2.html")
 
 
@@ -83,6 +113,44 @@ def admin_detail_delete(id):
         return redirect('/admin')
     except:
         return("При удалении произошла ошибка(")
+    
+
+@app.route('/admin/<int:id>/ready')
+def admin_detail_ready(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        processed_email = ProcessedEmail (
+            email = article.email,
+            original_id = article.id
+        )
+
+        db.session.add(processed_email)
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/admin')
+    except Exception as e:
+        app.logger.error('При добавлении в готовое произошла ошибка {e}')
+        return("Произошла ошибка при переносе")
+
+
+@app.route('/admin/ready')
+def admin_ready():
+    processed_email = ProcessedEmail.query.get(id)
+    return render_template("admin-ready-emails.html", email = processed_email)
+
+
+@app.route('/admin/ready/<int:id>/del')
+def admin_ready_del():
+    processed_email = ProcessedEmail.query.get(id)
+
+    try:
+        db.session.delete(processed_email)
+        db.session.commit()
+        return redirect('/admin/ready')
+    except:
+        return("При удалении готовой почты произошла ошибка(")
+
 
 
 @app.route('/test')
